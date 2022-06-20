38ab7de138b5e7c31fd3aeec3cc53f25d9bb9fc5
amateur-dev <me@dipeshsukhani.dev>
2022-06-21 07:24:58 +0800

    refactor

--- Foundry Test/src/MyToken.sol
+++ Foundry Test/src/MyToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20{
 
